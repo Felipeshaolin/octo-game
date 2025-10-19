7267e0d7a556aae86227b68e6a71bc93a9d16b7e
felipeshaolin <felipemfelsner@gmail.com>
2025-10-19 15:06:25 +0200

    create the first backend file

---
+++ back-end/back-end.cpp
@@ -0,0 +1,25 @@
+
+// this is the main file of teh back end of octo
+// octo is a simple minecraft like TUI game
+#include <string>
+
+class back-end {
+
+    public:
+
+    void get_terminal_info(){
+
+    }
+
+    void create_map(){
+
+    map = ""
+
+    }
+
+    private:
+
+    string map;
+
+}
+
