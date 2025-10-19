0e37bfa3e8f40785aa61e1aa5ccc46ded0a7da07
Felipeshaolin <felipemfelsner@gmail.com>
2025-10-19 23:01:13 +0200

    add core_game.txt to main folder

--- back-end/back-end.h
+++ back-end/back-end.h
@@ -10,11 +10,12 @@ class backend {
 
     public:
 
-        void start_ncurses();
+        void start_ncurses();// a function to start the ncurses system
 
-        void get_terminal_info();
+        void get_terminal_info();// initates the term_size_x and term_size_y variables
 
-        void create_map();
+        void create_map();// main algorithm to create the map
+
 
     private:
 
