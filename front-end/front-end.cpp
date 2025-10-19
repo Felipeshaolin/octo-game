48dcbf2cb576f61acd86dd2dd4979f2c8d99b26c
felipeshaolin <felipemfelsner@gmail.com>
2025-10-19 15:36:10 +0200

    expand back and front end files

---
+++ front-end/front-end.cpp
@@ -0,0 +1,18 @@
+
+// this is the main front end file for the front end of
+// octo a simple minecraft like TUI game
+
+#include <string>
+#include <ncurses.h>
+
+class front-end {
+
+    public:
+
+
+
+    private:
+
+}
+
+
