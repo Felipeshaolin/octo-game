bc69a0cd5d738131eb3b131b0cfa2c7a26c96c73
Felipeshaolin <felipemfelsner@gmail.com>
2025-10-20 14:53:32 +0200

    continue the developpment of backend.cpp

--- back-end/back-end.cpp
+++ back-end/back-end.cpp
@@ -3,23 +3,82 @@
 // octo is a simple minecraft like TUI game
 #include <string>
 #include <ncurses.h>
+#include <cstdlib>
 #include "back-end.h"
+
 using namespace std;
+                                                                                 
+//    ███    ██  ██████ ██    ██ ██████  ███████ ███████ ███████ 
+//    ████   ██ ██      ██    ██ ██   ██ ██      ██      ██      
+//    ██ ██  ██ ██      ██    ██ ██████  ███████ █████   ███████ 
+//    ██  ██ ██ ██      ██    ██ ██   ██      ██ ██           ██ 
+//    ██   ████  ██████  ██████  ██   ██ ███████ ███████ ███████                                                                                                                              
 
+void backend::start_ncurses(){ // a function to start the ncurses system
+    initscr();
+    cbreak();   // Disable line buffering
+    noecho();   // Disable automatic echoing of typed characters
+    curs_set(0); // Hide cursor
 
-void backend::start_ncurses(){initscr();} // a function to start the ncurses system
+} 
 
-void backend::get_terminal_info(){ // initates the term_size_x and term_size_y variables
+void backend::end_ncurses(){ // a function to stop the ncurses system
 
-    getmaxyx(stdscr, term_size_y , term_size_x );
+    curs_set(0); // Hide cursor
+    endwin();
 
 }
 
-void backend::create_grass_plane(){ // creates a map full of grass
+void backend::get_terminal_info(){ // initates the term_size_x and term_size_y variables
 
-    for (int number = 0; number <= this->term_size_x * this->term_size_y; number++ ) { 
+    getmaxyx(stdscr, this->term_size_y , this->term_size_x );
 
-        this->map.append("g");
+    // check if your terminal is small enough
+    //if (this->term_size_y > this->term_max_lenght_y | this->term_size_y == 0){ 
+
+        //this->term_size_y = this->term_max_lenght_y;
+    //}
+
+    //if (this->term_size_x > this->term_max_lenght_x | this->term_size_x == 0){
+
+        //this->term_size_x = this->term_max_lenght_x;
+    //}
+
+
+}
+
+
+//    ███    ███  █████  ██████  
+//    ████  ████ ██   ██ ██   ██ 
+//    ██ ████ ██ ███████ ██████  
+//    ██  ██  ██ ██   ██ ██      
+//    ██      ██ ██   ██ ██                                                                 
+
+void backend::create_grass_plane(){ // creates a map full of grass and initialises map size variable
+
+    this->map_size = this->term_size_x * this->term_size_y  ;
+
+    this->map = string(this->map_size-10, 'g');
+
+    //printw("Terminal size: %d x %d\n", term_size_x, term_size_y);
+    //refresh();  
+
+    printw( this->map.c_str() );
+
+    refresh();  // Refresh the screen to show the map
+
+}
+
+void backend::create_rock_formations(){ // creates the rocks on the map
+
+    for (int numb = 0; numb < this->number_of_rocks; numb++ ){ // repeat this for the number of rocks needed
+
+        int random_coord_x = rand() % ( this->term_size_x + 1 ) ;// get random x and y coordinates for the rock formation
+        int random_coord_y = rand() % ( this->term_size_y + 1 ) ; 
+
+        int rock_coordinates = random_coord_y * this->term_size_x  + random_coord_x ; // convert the 2d coordinate into a 1d coordinate
+
+        this->map[rock_coordinates] = 'R' ; // big rock is placed
 
     }
 
@@ -33,8 +92,34 @@ void backend::create_map(){ // main algorithm to create the map
 
 }
 
+//    ██ ███    ██ ██████  ██    ██ ████████ 
+//    ██ ████   ██ ██   ██ ██    ██    ██    
+//    ██ ██ ██  ██ ██████  ██    ██    ██    
+//    ██ ██  ██ ██ ██      ██    ██    ██    
+//    ██ ██   ████ ██       ██████     ██                                                                                       
+
 int backend::get_input(){
 
     return getch();
+
+}
+
+
+//test
+int main(){
+
+    backend object;
+
+    object.start_ncurses();
+
+    object.get_terminal_info();
+
+    object.create_grass_plane();
+
+    object.get_input();
+
+    object.end_ncurses();
+
+    return 0;
 
 }
--- back-end/back-end.h
+++ back-end/back-end.h
@@ -10,22 +10,40 @@ class backend {
 
     public:
 
+        // ncurses
+
         void start_ncurses();// a function to start the ncurses system
+
+        void end_ncurses();// a function to stop the ncurses system
 
         void get_terminal_info();// initates the term_size_x and term_size_y variables
 
-        void backend::create_grass_plane(); // creates a map full of grass
+        // map creation
+
+        void create_grass_plane(); // creates a map full of grass and initialises map size variable
+
+        void create_rock_formations(); // creates the rocks on the map
 
         void create_map();// main algorithm to create the map
+
+        //input
 
         int get_input();// returns a number based on the input that is being pressed
 
     private:
 
-         int term_size_x;
+        int term_size_x;
 
-         int term_size_y;
+        int term_size_y;
 
         string map;
+
+        int map_size;
+
+        int number_of_rocks = 10;
+
+        int term_max_lenght_x = 100;
+
+        int term_max_lenght_y = 100;
 
 };
