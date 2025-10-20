e33b907f97ad13aac47b3cb149c554feb36bea62
Felipeshaolin <felipemfelsner@gmail.com>
2025-10-20 15:33:14 +0200

    further expand map creation

--- back-end/back-end.cpp
+++ back-end/back-end.cpp
@@ -8,95 +8,158 @@
 
 using namespace std;
                                                                                  
-//    ███    ██  ██████ ██    ██ ██████  ███████ ███████ ███████ 
-//    ████   ██ ██      ██    ██ ██   ██ ██      ██      ██      
-//    ██ ██  ██ ██      ██    ██ ██████  ███████ █████   ███████ 
-//    ██  ██ ██ ██      ██    ██ ██   ██      ██ ██           ██ 
-//    ██   ████  ██████  ██████  ██   ██ ███████ ███████ ███████                                                                                                                              
+//███    ██  ██████ ██    ██ ██████  ███████ ███████ ███████ 
+//████   ██ ██      ██    ██ ██   ██ ██      ██      ██      
+//██ ██  ██ ██      ██    ██ ██████  ███████ █████   ███████ 
+//██  ██ ██ ██      ██    ██ ██   ██      ██ ██           ██ 
+//██   ████  ██████  ██████  ██   ██ ███████ ███████ ███████                                                                                                                              
 
-void backend::start_ncurses(){ // a function to start the ncurses system
-    initscr();
-    cbreak();   // Disable line buffering
-    noecho();   // Disable automatic echoing of typed characters
-    curs_set(0); // Hide cursor
+    void backend::start_ncurses(){ // a function to start the ncurses system
+        initscr();
+        cbreak();   // Disable line buffering
+        noecho();   // Disable automatic echoing of typed characters
+        curs_set(0); // Hide cursor
 
-} 
+    } 
 
-void backend::end_ncurses(){ // a function to stop the ncurses system
+    void backend::end_ncurses(){ // a function to stop the ncurses system
 
-    curs_set(0); // Hide cursor
-    endwin();
-
-}
-
-void backend::get_terminal_info(){ // initates the term_size_x and term_size_y variables
-
-    getmaxyx(stdscr, this->term_size_y , this->term_size_x );
-
-    // check if your terminal is small enough
-    //if (this->term_size_y > this->term_max_lenght_y | this->term_size_y == 0){ 
-
-        //this->term_size_y = this->term_max_lenght_y;
-    //}
-
-    //if (this->term_size_x > this->term_max_lenght_x | this->term_size_x == 0){
-
-        //this->term_size_x = this->term_max_lenght_x;
-    //}
-
-
-}
-
-
-//    ███    ███  █████  ██████  
-//    ████  ████ ██   ██ ██   ██ 
-//    ██ ████ ██ ███████ ██████  
-//    ██  ██  ██ ██   ██ ██      
-//    ██      ██ ██   ██ ██                                                                 
-
-void backend::create_grass_plane(){ // creates a map full of grass and initialises map size variable
-
-    this->map_size = this->term_size_x * this->term_size_y  ;
-
-    this->map = string(this->map_size-10, 'g');
-
-    //printw("Terminal size: %d x %d\n", term_size_x, term_size_y);
-    //refresh();  
-
-    printw( this->map.c_str() );
-
-    refresh();  // Refresh the screen to show the map
-
-}
-
-void backend::create_rock_formations(){ // creates the rocks on the map
-
-    for (int numb = 0; numb < this->number_of_rocks; numb++ ){ // repeat this for the number of rocks needed
-
-        int random_coord_x = rand() % ( this->term_size_x + 1 ) ;// get random x and y coordinates for the rock formation
-        int random_coord_y = rand() % ( this->term_size_y + 1 ) ; 
-
-        int rock_coordinates = random_coord_y * this->term_size_x  + random_coord_x ; // convert the 2d coordinate into a 1d coordinate
-
-        this->map[rock_coordinates] = 'R' ; // big rock is placed
+        curs_set(0); // Hide cursor
+        endwin();
 
     }
 
-}
+    void backend::get_terminal_info(){ // initates the term_size_x and term_size_y variables
 
-void backend::create_map(){ // main algorithm to create the map
+        getmaxyx(stdscr, this->term_size_y , this->term_size_x );
 
-    this->get_terminal_info();//get terminal info
-    
-    this->create_grass_plane();//create a map full of grass
+        // check if your terminal is small enough
+        if (this->term_size_y > this->term_max_lenght_y | this->term_size_y == 0){ 
 
-}
+            this->term_size_y = this->term_max_lenght_y;
+        }
 
-//    ██ ███    ██ ██████  ██    ██ ████████ 
-//    ██ ████   ██ ██   ██ ██    ██    ██    
-//    ██ ██ ██  ██ ██████  ██    ██    ██    
-//    ██ ██  ██ ██ ██      ██    ██    ██    
-//    ██ ██   ████ ██       ██████     ██                                                                                       
+        if (this->term_size_x > this->term_max_lenght_x | this->term_size_x == 0){
+
+            this->term_size_x = this->term_max_lenght_x;
+        }
+
+
+    }
+
+
+//███    ███  █████  ██████  
+//████  ████ ██   ██ ██   ██ 
+//██ ████ ██ ███████ ██████  
+//██  ██  ██ ██   ██ ██      
+//██      ██ ██   ██ ██                                                                 
+
+    void backend::set_concentration_of_obstacles(){// sets the maximum value of rocks/grass/water bodies
+        
+        int percentage = (this->map_size/100)* 5;
+
+        this->number_of_water = percentage;
+        this->number_of_grass = percentage;
+        this->number_of_rocks = percentage;
+
+    } 
+
+    void backend::create_grass_plane(){ // creates a map full of grass and initialises map size variable
+
+        this->map_size = this->term_size_x * this->term_size_y  ;
+
+        this->map = string(this->map_size, 'g');
+
+    }
+
+    void backend::create_rock_formations(){ // creates the rocks on the map
+
+        for (int numb = 0; numb < this->number_of_rocks; numb++ ){ // repeat this for the number of rocks needed
+
+            int random_coord_x = rand() % ( this->term_size_x + 1 ) ;// get random x and y coordinates for the rock formation
+            int random_coord_y = rand() % ( this->term_size_y + 1 ) ; 
+
+            int rock_coordinates = random_coord_y * this->term_size_x  + random_coord_x ; // convert the 2d coordinate into a 1d coordinate
+
+            this->map[rock_coordinates] = 'R' ; // big rock is placed
+
+        }
+
+    }
+
+    void backend::create_water_formation(){// places water on the map
+
+        // NOTE: THIS IS PLACEHOLDER CODE, I WILL CHANGE THE WATER CREATION AFTER
+        // TODO: change this code
+
+        for (int numb = 0; numb < this->number_of_water; numb++ ){ // repeat this for the number of water needed
+
+            int random_coord_x = rand() % ( this->term_size_x + 1 ) ;// get random x and y coordinates for the water formation
+            int random_coord_y = rand() % ( this->term_size_y + 1 ) ; 
+
+            int water_coordinates = random_coord_y * this->term_size_x  + random_coord_x ; // convert the 2d coordinate into a 1d coordinate
+
+            if (this->map[water_coordinates] != 'R'){
+
+                this->map[water_coordinates] = 'w' ; // big water is placed if rock not in the way
+
+            }
+
+        }
+
+    }
+
+    void backend::create_mountains(){// places water on the map
+
+        for (int numb = 0; numb < this->number_of_grass; numb++ ){ // repeat this for the number of rocks needed
+
+            int random_coord_x = rand() % ( this->term_size_x + 1 ) ;// get random x and y coordinates for the rock formation
+            int random_coord_y = rand() % ( this->term_size_y + 1 ) ; 
+
+            int grass_coordinates = random_coord_y * this->term_size_x  + random_coord_x ; // convert the 2d coordinate into a 1d coordinate
+
+            if ( (this->map[grass_coordinates] != 'R') | (this->map[grass_coordinates] != 'w') ){
+
+                this->map[grass_coordinates] = 'G' ; // big grass is placed if rock or water not in the way
+
+            }
+
+        }
+
+    }
+
+    void backend::create_map(){ // main algorithm to create the map
+
+
+        this->get_terminal_info();//get terminal info
+
+        
+        this->create_grass_plane();//create a map full of grass
+
+        this->set_concentration_of_obstacles(); //set the right amount of stuff
+
+        this->create_rock_formations();//create the rocks
+
+        this->create_water_formation();//create the water
+
+        this-> create_mountains();
+
+        printw( this->map.c_str() );
+
+        refresh();  // Refresh the screen to show the map
+    }
+
+    string backend::get_map(){ //returns the map string
+
+        return this->map ;
+
+    }
+
+//██ ███    ██ ██████  ██    ██ ████████ 
+//██ ████   ██ ██   ██ ██    ██    ██    
+//██ ██ ██  ██ ██████  ██    ██    ██    
+//██ ██  ██ ██ ██      ██    ██    ██    
+//██ ██   ████ ██       ██████     ██                                                                                       
 
 int backend::get_input(){
 
@@ -114,7 +177,7 @@ int main(){
 
     object.get_terminal_info();
 
-    object.create_grass_plane();
+    object.create_map();
 
     object.get_input();
 
--- back-end/back-end.h
+++ back-end/back-end.h
@@ -12,38 +12,52 @@ class backend {
 
         // ncurses
 
-        void start_ncurses();// a function to start the ncurses system
+            void start_ncurses();// a function to start the ncurses system
 
-        void end_ncurses();// a function to stop the ncurses system
+            void end_ncurses();// a function to stop the ncurses system
 
-        void get_terminal_info();// initates the term_size_x and term_size_y variables
+            void get_terminal_info();// initates the term_size_x and term_size_y variables
 
-        // map creation
+        // map 
 
-        void create_grass_plane(); // creates a map full of grass and initialises map size variable
+            void set_concentration_of_obstacles(); // sets the maximum value of rocks/grass/water bodies
 
-        void create_rock_formations(); // creates the rocks on the map
+            void create_grass_plane(); // creates a map full of grass and initialises map size variable
 
-        void create_map();// main algorithm to create the map
+            void create_rock_formations(); // creates the rocks on the map
+
+            void create_mountains(); //creates mountain in the map
+
+            void create_map();// main algorithm to create the map
+
+            void create_water_formation();// places water on the map
+
+            string get_map(); //returns the map string
+
+            void change_map( char type , int x , int y );// changes a block in a certain coordinate
 
         //input
 
-        int get_input();// returns a number based on the input that is being pressed
+            int get_input();// returns a number based on the input that is being pressed
 
     private:
 
-        int term_size_x;
-
+        int term_size_x; //size of terminal x,y
         int term_size_y;
 
-        string map;
+        int term_max_lenght_x = 150; // maximum size of terminal in x,y
+        int term_max_lenght_y = 150;
 
-        int map_size;
+        int screen_percentage_x; // 1% of the terminal size in x,y
+        int screen_percentage_y;
 
-        int number_of_rocks = 10;
 
-        int term_max_lenght_x = 100;
+        string map; //the map string
+        int map_size; //lenght of map string
 
-        int term_max_lenght_y = 100;
+        int number_of_rocks; // number of rocks per map
+        int number_of_water; // number of waters per map
+        int number_of_grass; // number of grasses per map
+
 
 };
